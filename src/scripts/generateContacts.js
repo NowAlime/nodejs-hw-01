340966ec7ea6ee864b817ca8192c2231a882db5c
NowAlime <147753857+NowAlime@users.noreply.github.com>
2024-07-09 18:45:49 +0300

    add

--- src/scripts/generateContacts.js
+++ src/scripts/generateContacts.js
@@ -1,5 +1,36 @@
+import fs from 'fs/promises';
+import { createFakeContact } from '../utils/createFakeContact.js';
 import { PATH_DB } from '../constants/contacts.js';
 
-const generateContacts = async (number) => {};
+const generateContacts = async (number) => {
+  let currentContacts = [];
 
+  try {
+    const data = await fs.readFile(PATH_DB, 'utf-8');
+    currentContacts = data ? JSON.parse(data) : []; 
+  } catch (error) {
+    if (error.code === 'ENOENT') {
+      console.warn(`File ${PATH_DB} not found, will create a new one.`);
+    } else {
+      console.error(`Error occurred while reading the file ${PATH_DB}`, error);
+      return; 
+    }
+  }
+
+  console.log('Current contacts:', currentContacts);
+
+  const newContacts = [];
+  for (let i = 0; i < number; i++) {
+    newContacts.push(createFakeContact());
+  }
+
+  const updatedContacts = [...currentContacts, ...newContacts];
+
+  try {
+    await fs.writeFile(PATH_DB, JSON.stringify(updatedContacts, null, 2), 'utf-8');
+    console.log('New contacts added successfully!');
+  } catch (error) {
+    console.error(`Error when trying to write to the file ${PATH_DB}`, error);
+  }
+};
 generateContacts(5);
--- src/scripts/getAllContacts.js
+++ src/scripts/getAllContacts.js
@@ -1,5 +1,17 @@
 import { PATH_DB } from '../constants/contacts.js';
-
-export const getAllContacts = async () => {};
+import fs from 'fs/promises';
+export const getAllContacts = async () => {
+  let allContacts = [];
+  try {
+    const data = await fs.readFile(PATH_DB, 'utf-8');
+    allContacts = JSON.parse(data);
+  } catch (err) {
+    if (err.code === 'ENOENT') {
+      console.warn(`Can't find ${PATH_DB} creating new`);
+    }
+    console.error('error:', err);
+  }
+  return allContacts;
+};
 
 console.log(await getAllContacts());
